9dda554047937ebd64769b804883a65359591197
ChoiJunho <ajchoi0928@gmail.com>
2021-10-26 22:23:18 +0900

    HotFix: 파일 경로 존재하지 않으면 생성되게 끔 수정

--- src/main/java/qr/generating/Generator.java
+++ src/main/java/qr/generating/Generator.java
@@ -12,17 +12,21 @@ import javax.imageio.stream.FileImageOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Generator implements Generatable {
-    
+
     /**
      * 보안 QR코드 생성 함수
-     * @param arr       사용할 해시, 암호화, 데이터가 저장되어있는 SecureQrCryptoArray
-     * @param authUrl   보안 QR 코드 인증에 사용할 URL
-     * @param c_index   사용할 해시, 암호화의 index
-     * @param d_index   사용할 데이터의 index
-     * @param width     QR 코드 크기 조절
-     * @param height    QR 코드 크기 조절
+     *
+     * @param arr     사용할 해시, 암호화, 데이터가 저장되어있는 SecureQrCryptoArray
+     * @param authUrl 보안 QR 코드 인증에 사용할 URL
+     * @param c_index 사용할 해시, 암호화의 index
+     * @param d_index 사용할 데이터의 index
+     * @param width   QR 코드 크기 조절
+     * @param height  QR 코드 크기 조절
      * @return 보안 QR의 byte array
      */
     @Override
@@ -57,16 +61,22 @@ public class Generator implements Generatable {
 
     /**
      * byte arrary 를 이미지 파일로 저장하는 함수
+     * 파일경로를 받아서, 만약 부모 경로가 존재하지 않으면 새로 만들고 파일을 저장함.
+     *
      * @param qr_byte_arr QR 코드의 byte array
-     * @param off 시작 오프셋
-     * @param path 저장 경로
+     * @param off         시작 오프셋
+     * @param path        저장 경로 (디렉토리+파일명)
      * @return 이미지 생성 성공 여부
      */
     @Override
     public boolean createSecureQRImage(byte[] qr_byte_arr, int off, String path) {
         boolean status;
         try {
-            FileImageOutputStream imageOutput = new FileImageOutputStream(new File(path));
+            File qrImg = new File(path);
+            if (!qrImg.getParentFile().exists()) {
+                qrImg.getParentFile().mkdirs();
+            }
+            FileImageOutputStream imageOutput = new FileImageOutputStream(qrImg);
             imageOutput.write(qr_byte_arr, off, qr_byte_arr.length);
             imageOutput.close();
             status = true;
--- src/test/java/server/GeneratorTest.java
+++ src/test/java/server/GeneratorTest.java
@@ -8,6 +8,9 @@ import crypto.SecureQrHashMD5;
 import junit.framework.TestCase;
 import qr.generating.Generator;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class GeneratorTest extends TestCase {
     /* QR 이미지 생성 요청시 생성 후 반환 테스트*/
@@ -35,5 +38,37 @@ public class GeneratorTest extends TestCase {
 
         /* 이미지 지정된 path 로 성공적으로 저장했다면 true */
         assertTrue(gen.createSecureQRImage(result, 0, "testImg.png"));
+    }
+
+    /* Testcase2  존재하지 않는 Path에 대해서 생성하려할때 */
+    public void testingPathExistence() throws Exception {
+        /* GSON 사용하면 CryptoArray없이 이미지 생성 가능 */
+        SecureQrCryptoArray arr = new SecureQrCryptoArray();    // Controller에 @Autowired 어노테이션과 함께 인스턴스 멤버로 선언
+        Generator gen = new Generator();
+
+        SecureQrCryptoAES256 aes256 = new SecureQrCryptoAES256();
+        aes256.setKey("00000000000000000000000000000000");
+
+        arr.add(new SecureQrHashMD5(), aes256); // 신규 QR 생성 요청 가정
+
+        /* QR 이미지 생성을 위해 요청한 데이터 */
+        String authUrl = "https://secureQR.com//v1//secureQR//";
+        String data = "https://github.com//SoTree17";
+        int c_index = 0;
+        int d_index = arr.addData(data);
+        int width = 200;
+        int height = 200;
+
+        /* Generator 호출 */
+        byte[] result = gen.createSecureQRCode(arr, authUrl, c_index, d_index, width, height);
+
+        /* 이미지 지정된 path 로 성공적으로 저장했다면 true */
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String dailyDirectory = dtf.format(now);
+        String path = "C:\\TestQR\\qrImg\\"+dailyDirectory+"\\";
+        String fileName = "testImg.png";
+
+        assertTrue(gen.createSecureQRImage(result, 0, path+fileName));
     }
 }
