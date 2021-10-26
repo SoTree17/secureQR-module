7acdd99c6fdc2f8ad4e6baad410625ce25e3e17c
Dongw1126 <topblade71@naver.com>
2021-10-26 18:53:05 +0900

    test: 해시 별 암호화 테스트 추가

--- src/test/java/crypto/SecureQrCryptoAES256Test.java
+++ src/test/java/crypto/SecureQrCryptoAES256Test.java
@@ -28,4 +28,29 @@ public class SecureQrCryptoAES256Test extends TestCase {
         assertEquals("CRYPTO", aes.getInstanceType());
         assertEquals("AES256", aes.getMethodType());
     }
+
+    public void testWithHash() throws Exception {
+        String origin = RandomString.getString(200);
+
+        boolean passed = true;
+
+        SecureQrCrypto aes256 = new SecureQrCryptoAES256();
+
+        SecureQrHash md5 = new SecureQrHashMD5();
+        SecureQrHash sha256 = new SecureQrHashSHA256();
+        SecureQrHash sha512 = new SecureQrHashSHA512();
+
+        SecureQrHash[] hashes = { md5, sha256, sha512 };
+
+        for(SecureQrHash h: hashes) {
+            String hashed = h.hashing(origin);
+            String cipherText = aes256.encrypt(hashed);
+            String decrypted = aes256.decrypt(cipherText);
+
+            if(!hashed.equals(decrypted)) {
+                passed = false;
+            }
+        }
+        assertTrue(passed);
+    }
 }
--- src/test/java/crypto/SecureQrCryptoRSATest.java
+++ src/test/java/crypto/SecureQrCryptoRSATest.java
@@ -32,4 +32,28 @@ public class SecureQrCryptoRSATest extends TestCase {
         assertEquals("CRYPTO", rsa.getInstanceType());
         assertEquals("RSA", rsa.getMethodType());
     }
+
+    public void testWithHash() throws Exception {
+        String origin = RandomString.getString(200);
+
+        boolean passed = true;
+
+        SecureQrCrypto rsa = new SecureQrCryptoRSA();
+
+        SecureQrHash md5 = new SecureQrHashMD5();
+        SecureQrHash sha256 = new SecureQrHashSHA256();
+        SecureQrHash sha512 = new SecureQrHashSHA512();
+
+        SecureQrHash[] hashes = { md5, sha256, sha512 };
+
+        for(SecureQrHash h: hashes) {
+            String hashed = h.hashing(origin);
+            String cipherText = rsa.encrypt(hashed);
+            String decrypted = rsa.decrypt(cipherText);
+            if(!hashed.equals(decrypted)) {
+                passed = false;
+            }
+        }
+        assertTrue(passed);
+    }
 }
